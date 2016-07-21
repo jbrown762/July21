15d772a6934a6ea2d4264881c96d9f3567f7f440
Joe Brown <jbrown762@gmail.com>
2016-07-21 14:19:26 -0400

    07/21/2016 2:19p

---
+++ src/com/brown/Notes.java
@@ -0,0 +1,12 @@
+package com.brown;
+
+public class Notes
+{
+	public static void main(String[] args)
+	{
+		for (int i = 0; i < 5; i++)
+		{
+
+		}
+	}
+}
--- src/com/brown/Whatever.java
+++ src/com/brown/Whatever.java
@@ -5,7 +5,11 @@ public class Whatever
 
 	public static void main(String[] args)
 	{
-		// :)
+
+		for (int i = 100; i > 0; i--)
+		{
+			System.out.println(i);
+		}
 
 	}
 
